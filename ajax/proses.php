296c20bc2b1f042e345f8852a62568499b92f299
murprakoso <prakoso.clan04@gmail.com>
2022-01-17 00:02:42 +0700

    init

---
+++ ajax/proses.php
@@ -0,0 +1,10 @@
+<?php
+
+$data = [
+    "Tokyo" => "Japan", "Mexico City" => "Mexico",
+    "New York City" => "USA", "Mumbai" => "India", "Seoul" => "Korea",
+    "Shanghai" => "China", "Lagos" => "Nigeria", "Buenos Aires" => "Argentina",
+    "Cairo" => "Egypt", "London" => "England"
+];
+
+echo json_encode($data);
